f6b2894debe8834abfbaf93faf2fabb62328e18d
연두 <108284815+yeondu1062@users.noreply.github.com>
2023-01-25 20:41:11 +0900

    Update yeondu.js

--- scripts/yeondu.js
+++ scripts/yeondu.js
@@ -1,7 +1,6 @@
 import { system, world } from "@minecraft/server";
 system.events.beforeWatchdogTerminate.subscribe(evd => evd.cancel = true);
 
-const max_enchant_level = 10; //최대 인챈트 레벨 제한
 const kick_message = '최대 인챈트 레벨 제한에 의해 추방당하셨습니다.'; //추방 메시지
 
 world.events.tick.subscribe(()=>{
@@ -10,7 +9,7 @@ world.events.tick.subscribe(()=>{
             const player_itemData = player.getComponent("minecraft:inventory").container.getItem(Math.floor(Math.random() * 9));
             const player_enchant = [...player_itemData.getComponent("minecraft:enchantments").enchantments];
             player_enchant.forEach(data => {
-                if(data.level > max_enchant_level) {
+                if(data.level > data.type.maxLevel) {
                     player.runCommandAsync(`tellraw @s {"rawtext":[{"text":"${player.nameTag}님이 ${kick_message} made by YEONDU"}]}`);
                     player.runCommandAsync(`kick @s ${kick_message + '\nmade by yeondu'}`);
                 }
